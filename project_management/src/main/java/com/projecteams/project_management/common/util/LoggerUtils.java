7c9f725d9c9e2006b39761c33d71c9a9956e5495
erudite_otto <142719407+eruditeotto@users.noreply.github.com>
2025-12-11 11:07:57 +0800

    chore: remove unused imports

--- project_management/src/main/java/com/projecteams/project_management/common/util/LoggerUtils.java
+++ project_management/src/main/java/com/projecteams/project_management/common/util/LoggerUtils.java
@@ -1,9 +1,6 @@
 package com.projecteams.project_management.common.util;
 
-import java.util.Objects;
-
 import lombok.experimental.UtilityClass;
-import lombok.extern.slf4j.Slf4j;
 
 @UtilityClass
 public class LoggerUtils {
