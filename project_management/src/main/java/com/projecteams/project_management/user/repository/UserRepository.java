024bdd42bd8e698b1ee71227e74426ccd5c80e0f
Sy-112004 <symoncleofe11@gmail.com>
2025-12-12 14:36:38 +0800

    feature:add user features

--- project_management/src/main/java/com/projecteams/project_management/user/repository/UserRepository.java
+++ project_management/src/main/java/com/projecteams/project_management/user/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.projecteams.project_management.user.repository;
 
-public class UserRepository {
+import com.projecteams.project_management.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
 }
